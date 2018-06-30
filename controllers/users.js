3d764a9624f245a4334be34b6a2684ef5b255bf6
Sanjeev Bw <sanjeev@busywizzy.com>
2018-06-30 13:11:45 +0530

    Vid 1-26 completed....

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,31 @@
 'use strict';
 
+const passport = require('passport');
 module.exports = function(_){
     
     return{
         SetRouting: function(router){
           router.get('/', this.indexPage);  
+          router.get('/signup', this.getSignUp);            
+          router.get('/home', this.homePage);            
+          router.post('/signup', this.postSignUp);            
         },
         
         indexPage: function(req, res){
             return res.render('index', {test: 'This is a test'});
         },
+        getSignUp:function(req, res){
+            return res.render('signup');
+        },
+        
+        postSignUp: passport.authenticate('local.signup', {
+            successRedirect: '/home',
+            failureRedirect: '/signup',
+            failureFlash: true
+        }),
+    
+        homePage: function(req, res) {
+            return res.render('home');
+        }
     }
 }
--- server.js
+++ server.js
@@ -2,10 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const http = require('http');
+
+const cookieParser = require('cookie-parser');
+const validator  = require('express-validator');
+const session = require('express-session');
+const MongoStore  =require('connect-mongo')(session);
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const passport = require('passport');
+
+
 const container = require('./container');
 
 
 container.resolve(function(users){
+    
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://localhost/footballkik');
+   // mongoose.connect('mongodb://localhost/footballkik', {useMongoClient:true});
+    
     const app = SetupExpress();
   
     function SetupExpress(){
@@ -24,9 +39,23 @@ container.resolve(function(users){
     
     
     function configureExpress(app){
+        require('./passport/passport-local')
         app.use(express.static('public'));
+        app.use(cookieParser());
         app.set('view engine', 'ejs');
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({extended: true}));
+        
+        app.use(validator());
+        app.use(session({
+            secret:'thisisasecretkey',
+            resave:true,
+            saveInitialized:true,
+            stroe: new MongoStore({mongooseConnection:mongoose.connection})
+        }))
+        
+        app.use(flash());
+        app.use(passport.initialize());
+        app.use(passport.session());
     }
 })
